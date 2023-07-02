e0202eb9138f9830efe3e70fc7b38c2874d79063
MarcShaXie <marcshaxie@gmail.com>
2023-07-02 11:07:27 -0400

    7: ChatGpt integration

--- ChatGpt API.py
+++ ChatGpt API.py
@@ -1,14 +1,33 @@
 import openai
 
-openai.api_key = "api-key"
-model_engine = "text-davinci-003" 
+# Set up your OpenAI API credentials
+openai.api_key = 'api-key'
 
-response = openai.ChatCompletion.create(
-    model='gpt-3.5-turbo',
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Hello, ChatGPT!"},
-    ])
+def ask_question(question):
+    # Define the prompt with the user's question
+    prompt = f"{question}\nAnswer:"
 
-message = response.choices[0]['message']
-print("{}: {}".format(message['role'], message['content']))
+    # Generate the response using OpenAI API
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.5
+    )
+
+    # Extract the answer from the API response
+    answer = response.choices[0].text.strip().split('\n')[0]
+
+    return answer
+
+# Main loop to keep asking questions until the user enters 'quit'
+while True:
+    user_question = input("Enter your question (or 'quit' to exit): ")
+    
+    if user_question.lower() == 'quit':
+        break
+
+    answer = ask_question(user_question)
+    print("Answer:", answer)
